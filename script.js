c06a51996ebd0be689b2450dafc341b4d6e30410
ShankarS97 <cecsshankars24@gmail.com>
2023-03-28 16:08:26 +0530

    shankar

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 'use strict'
 document.querySelector('.stud').addEventListener('click',function(){
-    document.querySelector('.fa').classList.add('hidden');
-    document.querySelector('.st').classList.remove('hidden');
-    
+    document.querySelector('.falogin').classList.add('hidden');
+    document.querySelector('.login').classList.remove('hidden');
 })
 document.querySelector('.faculty').addEventListener('click',function(){
-    document.querySelector('.st').classList.add('hidden');
-    document.querySelector('.fa').classList.remove('hidden');
+    document.querySelector('.login').classList.add('hidden');
+    document.querySelector('.falogin').classList.remove('hidden');
 })
-const body=document.querySelector('body');
+const body=document.querySelector('.body');
 body.addEventListener('click',function(e)
 {
     // console.log("1")
     // console.log(e.target);
-    if(e.target.classList.contains("body")){
-        
-        document.querySelector('.st').classList.add('hidden');
-        document.querySelector('.fa').classList.add('hidden');
-    }
+    if(e.target.classList.contains('hi')) return;
+ 
+        document.querySelector('.login').classList.add('hidden');
+        document.querySelector('.falogin').classList.add('hidden');
 //    console.log(e.target)
 //     console.log(e.target.classList.contains("login"))
 });
